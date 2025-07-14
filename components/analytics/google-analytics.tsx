98542328521da870b96954b9e9394eff76b0e82b
tomas <jacky.tomas.6688@gmail.com>
2025-07-14 08:13:36 -0700

    chore: 增加测试代码

--- components/analytics/google-analytics.tsx
+++ components/analytics/google-analytics.tsx
@@ -3,11 +3,15 @@
 import { GoogleAnalytics as NextGoogleAnalytics } from "@next/third-parties/google";
 
 export default function GoogleAnalytics() {
+  const analyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  window.__TEST__ = {
+    analyticsId,
+    NODE_ENV: process.env.NODE_ENV,
+  };
+  console.log("analyticsId", analyticsId, process.env.NODE_ENV );
   if (process.env.NODE_ENV !== "production") {
     return null;
   }
-
-  const analyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   if (!analyticsId) {
     return null;
   }
