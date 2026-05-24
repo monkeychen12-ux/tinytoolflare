7c7769436630dc1683a3c5df9069f44ec00320e1
tomas <jacky.tomas.6688@gmail.com>
2026-05-24 05:27:57 -0700

    fix i18n legal pathnames

--- i18n/locale.ts
+++ i18n/locale.ts
@@ -15,8 +15,6 @@ export const localeDetection =
   process.env.NEXT_PUBLIC_LOCALE_DETECTION === "true";
 
 export const pathnames = {
-  en: {
-    "privacy-policy": "/privacy-policy",
-    "terms-of-service": "/terms-of-service",
-  },
+  "/privacy-policy": "/privacy-policy",
+  "/terms-of-service": "/terms-of-service",
 } satisfies Pathnames<typeof locales>;
